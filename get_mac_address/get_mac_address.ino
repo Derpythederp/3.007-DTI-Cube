00e5a92ee6362fb638fa4c6d68931997597a8add
unknown <javier_2001@live.com.sg>
2022-03-31 05:05:55 +0800

    Restructed file hierarchy to prep for Prototype 2, not going to fork as similar code

--- get_mac_address/get_mac_address.ino
+++ get_mac_address/get_mac_address.ino
@@ -1,11 +1,17 @@
 // Complete Instructions to Get and Change ESP MAC Address: https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
 
-#include <ESP8266WiFi.h>
+#ifdef ESP32
+  #include <WiFi.h>
+  #define BOARD_NAME ESP32
+#else
+  #include <ESP8266WiFi.h>
+  #define BOARD_NAME ESP8266
+#endif
 
 void setup(){
   Serial.begin(115200);
   Serial.println();
-  Serial.print("ESP8266 Board MAC Address:  ");
+  Serial.print(String(BOARD_NAME) + " MAC Address:  ");
   Serial.println(WiFi.macAddress());
 }
  
