fc8c954bb8a7dd3003c20a71484f3e5b150b0724
unknown <javier_2001@live.com.sg>
2022-04-09 17:19:09 +0800

    updated p3 code

--- proto_3/esp32_main/esp32_main.ino
+++ proto_3/esp32_main/esp32_main.ino
@@ -1,16 +1,17 @@
-#include "hsvtorgb.h"
 #include <WiFi.h>
 #include <FastLED.h>
 #include <SPI.h>
 #include <SD.h>
+#include "read_write.h"
+#include "hsvtorgb.h"
 
-// #define COMMON_ANODE  // just to invert the calculations for RGB
 #define SLIDER_DIST 30
 #define SLIDER_COUNT 1
-#define BUTTON_COUNT 3
+#define BUTTON_COUNT 1
 #define PINGDELAY 100
 #define SOUND_MUL 0.0343
 #define COLOUR_STEP 0.5
+#define NUM_LEDS 10
 
 // Pins
 /* 
@@ -25,8 +26,10 @@ Blue - D25
 WS2812B
 --------
 Data - D27
+// Might have a second one
 If no one interacts with cube, i.e change to distance is below threshold and 
 the button is not pressed, the microcontroller turns the light to white.
+See if need to split the pins
 
 Ultrasound sensor for single slider(viewed from back)
 ----------------------
@@ -35,11 +38,7 @@ Shared Echo - D14
 
 Buttons
 -------
-BT1 - D15
-BT2 - D2
-BT3 - D4
-
-Should the buttons light up? To indicate which button to hit?
+BT1/BT2/BT3 - D15
 
 
 Speaker 
@@ -64,30 +63,58 @@ CS - D5
 
 */
 
-const int trigPins[SLIDER_COUNT] = {13};  // From front, left and right, middle tba
+const int trigPins[SLIDER_COUNT] = {13};  
 const int echoPin = 14;  
-const int buttonPins[BUTTON_COUNT] = {15, 2, 4};    // 35 and 34 doesn't have buttons yet
+const int buttonPins[BUTTON_COUNT] = {15};    
 const int speakerPin = 21;
 const int SD_MOSI = 23;
 const int SD_MISO = 19;
 const int SD_CLK = 18;
 const int SD_CS = 5;
-
+const int ledPin = 27;
+const uint8_t broadcastAddress[6] = {};  // to be added 
+bool buttonState = 0;
 
 // Stored states, buttons and hue value
+struct {
+  float hue;
+  float distances[SLIDER_COUNT];
+} soundncolors;  // for serializing data
+
 float hue;
-float distances[SLIDER_COUNT];  // distances in cm in order of sensor
+float distances[SLIDER_COUNT];  // distances in cm in order of sensor, slider count is the remote one actually
 unsigned long lastPing;
 volatile unsigned long startEcho;
 volatile unsigned long stopEcho;
-float old_rgb[3] = {128.0, 128.0, 128.0};
+float old_hue = 128.0;
 
 
 void ISR_ECHO();
+void ISR_BUTTON();
+void OnDataSent();
+void OnDataRecv();
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(OnDataRecv);
+
+  esp_now_peer_info_t peerInfo;  // set up buffer for peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
   
   for (int i = 0; i < SLIDER_COUNT; i++) {
     pinMode(trigPins[i], OUTPUT);
@@ -102,6 +129,9 @@ void setup() {
     Serial.println("SD Card initialization failure");
     return;
   }
+
+  Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+  Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
   
   File file = SD.open("/joke.txt")  // obtain file handle
   if (!file) {
@@ -111,25 +141,36 @@ void setup() {
   else {
     Serial.println("No one likes to listen to the same awful joke twice");
   }
+  
   file.close();  // ensure you close your file handle when you finish writing
   
   pinMode(echoPin, INPUT);  // shared echo pin
 
   pinMode(speakerPin, OUTPUT);
  
-  attachInterrupt(digitalPinToInterrupt(14), ISR_ECHO, CHANGE);  // interrupt CPU 0 when slider detects change, might cause button to halt
+  attachInterrupt(digitalPinToInterrupt(echoPin), ISR_ECHO, CHANGE);  // interrupt CPU 0 when slider detects change, might cause button to halt
+  attachInterrupt(digitalPinToInterrupt(buttonPin), ISR_BUTTON, FALLING);  // interrupt CPU 0 when button is released
   lastPing = millis();  // get current milisecond for delay later
+  FastLED.addLeds<WS2812B, ledPin, RGB>(leds, NUM_LEDS);
 }
 
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *soundncolors_data, int data_len) {
+  // If received button and sensor data, update struct of button and sensor data
+  memcpy(&soundncolors, &soundncolors_data, sizeof(soundncolors));
+}
 
 float distance2colourval(float dist) {
   // Expects a distance in cm
-  // Return normalized value from 0.0 to 1.0 if distance is less than max slider distance, else returns 0
-  return (dist < SLIDER_DIST) ? ((dist - 2) / (SLIDER_DIST - 2)) : 0;
+  // Return value from 0.0 to 255.0 if distance is less than max slider distance, else returns 0
+  return (dist < SLIDER_DIST) ? ((dist - 2) / (SLIDER_DIST - 2)) * 255 : 0;
 }
 
 void doMeasurement() {
-  // Expects a pointer to array of distances
   // Does measurement of distances, and returns an array of n distances
   unsigned long startWait;
   unsigned long timeout = 50;  // 23ms is 4m which is already out of range of sensor, we just gave it 27ms more than it needed.
@@ -158,28 +199,26 @@ void doMeasurement() {
   }
 }
 
-void setColours(float hue, float brightness) {
-  // expects a float representing hue from 0.0 to 1.0, and a float representing brightness from 0.0 to 1.0
-  // Will set the common cathode LED to the appropriate colour
 
-  float rgb[3];
-  hsv2rgb(hue, 1.0, brightness, rgb); 
+void checkButton() {
+  // Check if button has been pressed, and if it is, play sound if the timing is fufilled
+  if (buttonState = true) {
+    Serial.printf("Buttons has been pressed");  // add play sound here
+    buttonState ^= 1;
+  }
+}
 
-  #ifdef COMMON_ANODE
-    rgb[0] = 255 - rgb[0];
-    rgb[1] = 255 - rgb[1];
-    rgb[2] = 255 - rgb[2];
-  #endif
+void setColours(float hue) {
+  // expects a float representing hue from 0.0 to 255.0
+  // Will set the WS2812B to the appropriate colour
   
   // cross fade code
-  old_rgb[0] = (rgb[0] > old_rgb[0]) ? old_rgb[0] + COLOUR_STEP : old_rgb[0] - COLOUR_STEP;
-  old_rgb[1] = (rgb[1] > old_rgb[1]) ? old_rgb[1] + COLOUR_STEP : old_rgb[1] - COLOUR_STEP;
-  old_rgb[2] = (rgb[2] > old_rgb[2]) ? old_rgb[2] + COLOUR_STEP : old_rgb[2] - COLOUR_STEP;
-
-
-  ledcWrite(0, old_rgb[0]);
-  ledcWrite(1, old_rgb[1]);
-  ledcWrite(2, old_rgb[2]);
+  old_hue = (hue > old_hue) ? old_hue + COLOUR_STEP : old_hue - COLOUR_STEP;
+  
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i].setHue(old_hue);
+  }
+  FastLED.show();
 }
 
 void loop() {
@@ -190,16 +229,12 @@ void loop() {
     doMeasurement();
     float hue = distance2colourval(distances[0]);
     Serial.println(String("Slider Sensor: ") + distances[0] + " cm");  // print distance
-    setColours(hue, 1.0);
+    setColours(hue);
   }
-
+  
+  checkButton();
   // if (millis() - lastNote >= NOTEDELAY[CURR_NOTE]) && (randomButton ==  {
     // playWav(CURR_NOTE, speakerPin);
-
-  
-//  if (button_1 || button_2 || button_3) {
-//    Serial.println("Button was pressed!");
-//  }
 }
 
 
@@ -215,4 +250,8 @@ void IRAM_ATTR ISR_ECHO() {
     // Low state --> Stop timer for echo
     stopEcho = micros();
   }
+}
+
+void IRAM_ATTR ISR_BUTTON() {
+  buttonState ^= 1;
 }
--- proto_3/esp32_main/hsvtorgb.h
+++
@@ -1,4 +0,0 @@
-float fract(float x);
-float mix(float a, float b, float t);
-float* hsv2rgb(float h, float s, float b, float* rgb);  // rgb is an array for rgb values normalized to 0.0 to 1.0, hsb are also normalized
-float* rgb2hsv(float r, float g, float b, float* hsv);
